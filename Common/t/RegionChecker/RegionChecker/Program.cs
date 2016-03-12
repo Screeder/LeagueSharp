8eb73066c350f51a0c2bbacadc98de5566c2cd50
Screeder <luigipart@web.de>
2016-03-12 12:52:57 +0100

    Added SAssembliesLib

---
+++ Common/t/RegionChecker/RegionChecker/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionChecker
+{
+    using LeagueSharp;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Region: " + Game.Region);
+        }
+    }
+}
